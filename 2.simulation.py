5dbafed1948cc826606a84a626dc34e44a7838ec
Simon Aulet <simon.aulet@gmail.com>
2026-04-03 09:46:17 -0300

    begin plotting original signals

--- 2.simulation.py
+++ 2.simulation.py
@@ -18,7 +18,6 @@
 # Simulation of the signals, first in time domain
 #
 # %%
-
 import numpy as np
 from scipy import signal
 from matplotlib import pyplot as plt
@@ -43,10 +42,10 @@ t = np.arange(0, 1e-3, 1/fs)
 f0 = 5e6          # ramp freq =5MHz
 duty = 1          # symmetry = 100%
 
-x_1 = signal.sawtooth(2*np.pi*f0*t, width=duty)
-#x_1 = x_1 * 10**1.6
+x_1 = 2.45 * signal.sawtooth(2*np.pi*f0*t, width=duty)
+
 # %%
-plt.plot(t, x)
+plt.plot(t, x_1)
 plt.xlim(0.001e-3, 0.002e-3)
 plt.title('100% symmetry sawtooth Sample from 1 to 2 ms')
 plt.xlabel('Time (ms)')
@@ -56,12 +55,14 @@ plt.show()
 # %% [markdown]
 # Simulación del analizador de espectro. Falta la resolución del ancho del filtro que movimos en el laboratorio
 #
+
 # %%
+from spectrum import Periodogram, data_cosine
 
-
-# %% [markdown]
-# Ahora paso la señal por el analizador de espectro
-#
+# %%
+p = Periodogram(x_1, sampling=400)
+p.run()
+p.plot(marker='o')
 # %%
 
 def spectrum_analyzer(
@@ -69,8 +70,7 @@ def spectrum_analyzer(
     f_start, f_stop,
     n_points,
     R=50,
-    rbw_hz=100e3   # default: 100 kHz RBW (reasonable for ~200 MHz span)
-):
+    rbw_hz=100e3):   # default: 100 kHz RBW (reasonable for ~200 MHz span)
     """
     Simulates a spectrum analyzer.
 
@@ -101,9 +101,16 @@ def spectrum_analyzer(
     freqs = freqs[mask]
     X = X[mask]
 
-    # --- 3. Convert magnitude to power ---
-    mag = np.abs(X) / N
+    df = fs / N
+
+    # window corrections
+    cg = np.mean(window)
+    U = np.mean(window**2)
+
+    mag = np.abs(X) / (N * cg)
+
     P = (mag**2) / R
+    P = P * (rbw_hz / df) / U
 
     # Avoid log of zero
     P[P <= 0] = 1e-20
@@ -111,7 +118,6 @@ def spectrum_analyzer(
     P_dbm = 10 * np.log10(P / 1e-3)
 
     # --- 4. Simulate RBW (frequency-domain smoothing) ---
-    df = fs / N
     rbw_bins = rbw_hz / df
 
     P_dbm = gaussian_filter1d(P_dbm, sigma=rbw_bins)
@@ -126,13 +132,24 @@ def spectrum_analyzer(
     P_interp = np.interp(freqs_target, freqs_span, P_span)
 
     # --- 7. Output format ---
-    freq_ghz = freqs_target / 1e9
+    freq_ghz = freqs_target / 1e6
     processed_data = np.column_stack((freq_ghz, P_interp))
 
     return processed_data
 
 # %%
-ramp_sym_50_S = spectrum_analyzer(coupler(x), fs, 0, 200e6, 100000)
+ramp_sym_100_S = spectrum_analyzer(coupler(x_1), fs, f_start=0, f_stop=200e6, n_points = 100000)
 
 # %%
-plt.plot(ramp_sym_50_S[:, 0], ramp_sym_50_S[:, 1])
+plt.plot(ramp_sym_100_S[:, 0], ramp_sym_100_S[:, 1])
+plt.title('100% symmetry sawtooth spectrum simulation')
+plt.grid(True)
+plt.xlabel('Frequency (MHz)')
+plt.ylabel('Power (dBm)')
+#plt.xlim(0, 15)
+plt.show()
+
+# %%
+x_1[0:100]
+# %%
+ramp_sym_100_S[0:100]
